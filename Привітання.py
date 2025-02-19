0745f10d08435a8cf10e6016a4d8877aa7c1f5ca
Ublo1r <borisul406@gmail.com>
2025-02-19 18:02:37 +0200

    Додав файл

---
+++ Привітання.py
@@ -0,0 +1,5 @@
+from tkinter import *
+def button_clicked():
+    print("Hello! I you are Djris Ulianow Dmitriewich. Buy!")
+button1=Button(text="Pick up me", command=button_clicked)
+button1.pack()
